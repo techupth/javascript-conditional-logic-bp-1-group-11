3754927b55b03acc160f79ca16a17acbbff53346
MSI\llxlust <llxlust.dev@gmail.com>
2023-09-26 13:34:47 +0700

    Ton's work

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,5 @@
 function calculateStudentGrade(score) {
-  // Start coding here
+  return score >=90 && score <= 100 ? "A" :  score >= 80  && score <= 89 ? "B" : score >= 70   && score <= 79 ? "C" : score >= 60   && score <= 69 ? "D" : "F"
 }
 
 // Example case
@@ -9,7 +9,7 @@ let janeGrade = calculateStudentGrade(80);
 let jjGrade = calculateStudentGrade(60);
 let alexGrade = calculateStudentGrade(90);
 
-console.log(`John grade is ${johnGrade}`);
+console.log(`John grade is ${johnGrade}`); 
 console.log(`Lisa grade is ${lisaGrade}`);
 console.log(`Jane grade is ${janeGrade}`);
 console.log(`JJ grade is ${jjGrade}`);
